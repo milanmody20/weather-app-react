c614397875a4f0074e56e7cd7befe6f6b145decb
Milan <milanm1120@gmail.com>
2022-05-18 21:13:30 -0400

    create test SearchBar.js file

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render(){
+        return(
+            <div>
+                This is the search bar component
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
