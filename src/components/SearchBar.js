82092adc5b741c877c91c8c26e820df9d373a6b7
Milan <milanm1120@gmail.com>
2022-05-18 21:35:39 -0400

    completed search bar by adding state management and preventing rerendering on submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: "",
+        };
+    };
+
+    onInputChange(e) {
+        this.setState({
+            location: e.target.value
+        })
+        console.log(e.target.value);
+    }
+
+    onFormSubmit(e){
+        e.preventDefault();
+    }
+
     render(){
+        const location = this.state.location;
         return(
             <div>
-                This is the search bar component
+                <form onSubmit={(e) => this.onFormSubmit(e)}>
+                    <button type = "submit">Search</button>
+                    <input id="search" name="search" value={location} onChange={(e) => this.onInputChange(e)}></input>
+                </form>
             </div>
         )
     }
